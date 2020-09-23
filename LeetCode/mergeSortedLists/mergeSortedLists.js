735ccf404f5617ab38ba9191feb448c7b9cb7664
Megan Leaf <meganbleaf@gmail.com>
2020-09-23 14:44:21 -0500

    start: just started working on merging 2 sorted lists problem

---
+++ LeetCode/mergeSortedLists/mergeSortedLists.js
@@ -0,0 +1,16 @@
+//approach: check each head.value to see if its greater than or equal to each other
+//if equal to, add both to the first linkedlist
+//if less than, add it to the linkedList in place of the greater one
+
+// const mergeSortedLists = (headOne, headTwo) => {
+//     let pointer1 = headOne
+//     let pointer2 = headTwo
+//     let current
+
+//     if (headOne.value === headTwo.value) {
+//         pointer1 = headOne.next //we're holding the rest of the list here
+//         headOne.next = headTwo
+//     }
+// }
+
+// ^^ coming back to this one!
