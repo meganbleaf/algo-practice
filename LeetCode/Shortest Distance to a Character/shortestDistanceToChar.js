a36da89b6aa03cfee3713c11a240bc4a45b7cb4e
Megan Leaf <meganbleaf@gmail.com>
2020-09-23 18:56:00 -0500

    solve: shortest distance to character solved

--- LeetCode/Shortest Distance to a Character/shortestDistanceToChar.js
+++ LeetCode/Shortest Distance to a Character/shortestDistanceToChar.js
@@ -1,20 +1,18 @@
 const shortestToChar = (string, letter) => {
     let arrayOfIndeces = []
     stringArray = string.split('')
-    //if string[i] is equal to letter - push the index up to the array of indeces
     for (let i = 0; i < stringArray.length; i++) {
-        if (string[i] === letter) {
+        if (stringArray[i] === letter) {
             arrayOfIndeces.push(i)
         }
     }
-    //[3, 5, 6, 11]
 
     let arrayOfShortPaths = []
 
 
     for (let i = 0; i < stringArray.length; i++) {
         let shortestPath = Infinity
-        arrayOfIndeces.forEach((char, index) => {
+        arrayOfIndeces.forEach((index) => {
 
             let path = Math.abs(index - i)
             if (path < shortestPath) {
@@ -23,18 +21,6 @@ const shortestToChar = (string, letter) => {
         })
         arrayOfShortPaths.push(shortestPath)
     }
-    // stringArray.forEach((char, index) => {
-    //     for (let i = 0; i < arrayOfIndeces.length; i++) {
-    //         if ((arrayOfIndeces[i] - index) < shortestPath) {
-    //             let path = Math.abs(index - arrayOfIndeces[i])
-    //             if (path < shortestPath) {
-    //                 shortestPath = path
-    //             }
-    //         }
-    //         arrayOfShortPaths.push(shortestPath)
-    //     }
-
-    // })
 
     return arrayOfShortPaths
 }
@@ -44,14 +30,3 @@ console.log(shortestToChar("loveleetcode", 'e'))
 
 
 
-// let checkPath = index - arrayOfIndeces[0]
-        // for (let i = 0; i < arrayOfIndeces.length; i++) {
-        //     let checkPath = arrayOfIndeces[i] - index
-        //     if (checkPath === 0) {
-        //         arrayOfShortPaths.push(checkPath)
-        //         continue
-        //     }
-        //     if (checkPath > 0) {
-        //         arrayOfShortPaths.push(checkPath)
-        //     }
-        // }
