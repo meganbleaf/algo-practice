39f997477e9059dee9c348f437b58d5d394e3954
Megan Leaf <meganbleaf@gmail.com>
2020-09-23 23:17:13 -0500

    solve: solution for levelwidth

--- AlgoCasts/exercises/levelwidth/index.js
+++ AlgoCasts/exercises/levelwidth/index.js
@@ -11,6 +11,23 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    let counter = [0]
+    let arrayOfNodes = [root, 's']
+    while (arrayOfNodes.length > 1) {
+        if (arrayOfNodes[0] === 's') {
+            arrayOfNodes.shift()
+            counter.push(0)
+            arrayOfNodes.push('s')
+        } else {
+            arrayOfNodes.push(...arrayOfNodes[0].children)
+            arrayOfNodes.shift()
+            counter[counter.length - 1] += 1
+        }
+
+    }
+    return counter
+}
+
 
 module.exports = levelWidth;
