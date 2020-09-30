87ba3b8d80433c6cf0286cd8336dc588adc637ec
Megan Leaf <meganbleaf@gmail.com>
2020-09-29 23:41:26 -0500

    added: some code but not a working solution to merge sorted lists

--- LeetCode/mergeSortedLists/mergeSortedLists.js
+++ LeetCode/mergeSortedLists/mergeSortedLists.js
@@ -14,3 +14,29 @@
 // }
 
 // ^^ coming back to this one!
+
+function mergeSortedLists(head1, head2) {
+    let p1a = head1
+    let p1b = head1.next
+    let p2 = head2
+    //insert between
+    if (p1a.v <= p2.v && p1b.v >= p2.v) {
+        p1a.next = p2
+        p2 = p2.next
+        p1a = p1a.next
+        p1a.next = p1b
+
+        //insert before
+    } else if (p1a.v > p2.v) {
+        p1b = p1a
+        p1a = p2
+        p2 = p2.next
+        p1a.next = p1b
+    } else if (p2.v > p1b.v) {
+        while (p2.v > p1b.v && p1b.next !== null) {
+            p1b = p1b.next
+            p1a = p1a.next
+        }
+        p1b.next = p2
+    }
+}
